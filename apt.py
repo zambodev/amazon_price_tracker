0d00e1e91ab3678b259bf7a0c760845d7ece4fc3
Zambo_dev <thomasz01@outlook.it>
2022-11-15 11:05:00 +0100

    Added storic price

--- apt.py
+++ apt.py
@@ -7,24 +7,26 @@ from bs4 import BeautifulSoup
 
 if __name__ == "__main__":
 
-	with open(sys.argv[1], "r") as file:
-		data = file.read()
+    with open(sys.argv[1], "r") as file:
+        data = file.read()
 
-	obj = json.loads(data)
+    obj = json.loads(data)
 
-	for chunk in obj:
-		url = chunk['url']
+    for chunk in obj:
+        url = chunk['url']
 
-		page = requests.get(url,headers={"User-Agent":"Defined"})
-		soup = BeautifulSoup(page.content, "html.parser")
-		name = soup.find("span", {"id":"productTitle"}).get_text().lstrip()
-		price = soup.find("span", {"class":"a-offscreen"}).get_text()
-		limit = len(name) if len(name) < 60 else 60
-		old_price = chunk['old_price']
-		print("Object: {}\nOld price: {} Price: {}\n".format(name, old_price, price))
+        page = requests.get(url,headers={"User-Agent":"Defined"})
+        soup = BeautifulSoup(page.content, "html.parser")
+        name = soup.find("span", {"id":"productTitle"}).get_text().lstrip()
+        price = soup.find("span", {"class":"a-offscreen"}).get_text()
 
-		chunk['old_price'] = price
-		chunk['price'] = old_price
+        price_list = chunk['price_list']
+        min_price = None if not price_list else min(price_list)
+        print("Object: {}\nBest price: {} Price: {}\n".format(name, min_price, price))
 
-	with open(sys.argv[1], "w") as file:
-		json.dump(obj, file)
+        if not price in price_list:
+            price_list.append(price)
+            chunk['price_list'] = price_list
+
+    with open(sys.argv[1], "w") as file:
+        json.dump(obj, file)

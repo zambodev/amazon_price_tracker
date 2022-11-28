d29b0a5ddee8e065e8f86b27652a52cb56b83b16
zambodev <thomasz01@outlook.it>
2022-11-28 09:43:48 +0100

    Argv fix

--- apt.py
+++ apt.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     obj = json.loads(data)
 
-    if sys.argv[2]:
+    if len(sys.argv) > 2:
         obj.append({"url":sys.argv[2],"price_list":[]})
 
     header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"}

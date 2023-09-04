0277da5316d05762d67e266470eec0b942dddfdb
zambodev <thomaszambelli01@gmail.com>
2023-09-04 11:51:03 +0200

    try/catch

--- apt.py
+++ apt.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import requests
 import sys
@@ -15,56 +15,58 @@ if __name__ == "__main__":
         sys.exit()
 
     with open(sys.argv[1], "r") as file:
-        obj = json.loads(file.read())
+        obj_list = json.loads(file.read())
 
     if len(sys.argv) > 2:
         for link in sys.argv[2:]:
             linksplit = link.split(":", 1)
-            obj.append({"title":linksplit[0],"url":linksplit[1],"price_list":[]})
+            obj_list.append({"title":linksplit[0],"url":linksplit[1],"price_list":[]})
 
     header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36"}
+	
+    for obj in obj_list:
 
-    for chunk in obj:
-        url = chunk['url']
+        title = obj['title']
+        url = obj['url']
 
-        print(f"Object: {chunk['title']}")
         try:
             page = requests.get(url, headers=header, timeout=60)
             if(page.status_code != 200): 
                 continue
-        except:
-            print("ERROR: Item page currently unavailable! Try again later.\n")
-            continue
-            
 
-        soup = BeautifulSoup(page.content, "html.parser")
+            soup = BeautifulSoup(page.content, "html.parser")
 
-        price_text = soup.find("span", {"class":"a-offscreen"})
-        if(not price_text):
-            print("ERROR: Item data non loaded correctly")
-            continue
+            price_text = soup.find("span", {"class":"a-offscreen"})
+            if(not price_text):
+                print("ERROR: Item data non loaded correctly")
+                continue
 
-        price = float(re.findall(r"[-+]?(?:\d*\.*\d+)", price_text.get_text().replace(',','.'))[0])
-        try:
+            price = float(re.findall(r"[-+]?(?:\d*\.*\d+)", price_text.get_text().replace(',','.'))[0])
             sign = soup.find("span", {"class":"a-price-symbol"}).get_text()
+
+            price_list_str = obj['price_list']
+            price_list = [float(i.replace(',','.')) for i in price_list_str]
+
+            min_price = None if not price_list else format(float(min(price_list)), '.2f')
+            avg_price = None if not price_list else format(float(fmean(price_list)), '.2f')
+            max_price = None if not price_list else format(float(max(price_list)), '.2f')
+
+            print(f"\n{title}\n")
+            print(f"Range: {min_price} - {max_price} {sign}")
+            print(f"Avg: {avg_price} {sign}")
+            print(f"Latest: {price} {sign}")
+
+            if not str(price) in price_list_str:
+                price_list_str.append(str(price))
+                obj['price_list'] = price_list_str
+    
+            print("--------------------------------")
+
         except:
-            sign = '$/€';
-        price_list_str = chunk['price_list']
-        price_list = [float(i.replace(',','.')) for i in price_list_str]
+            print(f"\nERROR: Item '{title}' page currently unavailable! Try again later.")
+            print("--------------------------------")
+            continue
 
-        min_price = None if not price_list else format(float(min(price_list)), '.2f')
-        avg_price = None if not price_list else format(float(fmean(price_list)), '.2f')
-        max_price = None if not price_list else format(float(max(price_list)), '.2f')
-
-        print(f"Range: {min_price} - {max_price} {sign}")
-        print(f"Avg: {avg_price} {sign}")
-        print(f"Latest: {price} {sign}")
-        print("--------------------------------")
-
-        if not str(price) in price_list_str:
-            price_list_str.append(str(price))
-            chunk['price_list'] = price_list_str
 
     with open(sys.argv[1], "w") as file:
-        file.write(json.dumps(obj, indent=4))
-
+        file.write(json.dumps(obj_list, indent=3))
